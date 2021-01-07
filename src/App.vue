e8af1eca29b67e1b53ee689fdc49a38d18f99784
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2021-01-08 00:36:20 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <h1 id="title">Etkot Kalenteri</h1>
+        <div id="new-button">
+            <p id="button-text">NEW</p>
+        </div>
+        <div id="container">
+            <Event title="Pikkujoulusauna" />
+            <Event title="Pajailta" />
+            <Event title="Etkot-Kyykkä" />
+            <Event title="Pietarin reissu" />
+            <Event title="Minuuttipeli" />
+            <Event title="Keilausiltama" />
+            <Event title="Kaljagolf" />
+            <Event title="Random ryybs ilta" />
+        </div>
+    </div>
+</template>
+
+<script>
+import Event from './components/Event'
+
+export default {
+    name: 'App',
+    components: {
+        Event
+    }
+}
+</script>
+
+<style>
+* {
+    padding: 0;
+    margin: 0;
+}
+body {
+    background-color: #1c1c1c;
+}
+#title {
+    color: #f700ff;
+    font-family: 'NSimSun';
+    text-decoration: underline;
+    font-size: 340%;
+    padding: 45px;
+}
+#new-button {
+    background-color: #f700ff;
+    border-radius: 10px;
+    width: 120px;
+    height: 50px;
+    margin: 50px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transition: 0.5s;
+}
+#new-button:hover {
+    width: 125px;
+    height: 52px;
+    margin: 48px;
+}
+#button-text {
+    text-align: center;
+    color: white;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande',
+        'Lucida Sans Unicode', Geneva, Verdana, sans-serif1;
+    font-size: 25px;
+    padding: 6px;
+    cursor: pointer;
+}
+#container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-left: 70px;
+    margin-right: 70px;
+    margin-top: 25px;
+}
+</style>
---
+++ src/components/Event.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="box" :style="{ height: cardOpen ? '250px' : '80px' }">
+        <p id="name">{{ title }}</p>
+        <div id="button">
+            <p v-if="!cardOpen" id="plus" v-on:click="infoPressed">+</p>
+            <p v-else id="minus" v-on:click="infoPressed">-</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Event',
+    props: {
+        title: String
+    },
+    data() {
+        return {
+            cardOpen: false
+        }
+    },
+    methods: {
+        infoPressed() {
+            this.cardOpen = !this.cardOpen
+        }
+    }
+}
+</script>
+
+<style scoped>
+#box {
+    background-color: #404040;
+    min-width: 200px;
+    padding: 30px;
+    padding-bottom: 15px;
+    margin: 25px;
+    border-radius: 10px;
+    transition: 0.5s;
+}
+#name {
+    color: white;
+    font-size: 30px;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    text-align: center;
+}
+#button {
+    background-color: grey;
+    border-radius: 100%;
+    width: 25px;
+    height: 25px;
+    margin: auto;
+    margin-top: 10px;
+    transition: 0.5s;
+}
+#plus {
+    color: white;
+    font-size: 25px;
+    text-align: center;
+    cursor: pointer;
+}
+#minus {
+    color: white;
+    font-size: 20px;
+    text-align: center;
+    cursor: pointer;
+}
+</style>
